44838548b8e31f220f602a5348ecea43e18b9fe5
wuwei <wudawei112345>
2020-11-15 14:36:55 +0800

    ArrayBoundedListImpl 类实现完成 提交

--- src/com/pro/imple/ArrayBoundedListImpl.java
+++ src/com/pro/imple/ArrayBoundedListImpl.java
@@ -9,47 +9,126 @@ import com.pro.inter.ListInterface;
  * @author wuwei
  */
 public class ArrayBoundedListImpl<T> implements ListInterface<T> {
+	
+	private static final int DEFAULT_MAX_SIZE = 10;
+	private Object[] elements;
+	private int elementsNum = 0;
+	
+	public ArrayBoundedListImpl() {
+		elements = new Object[DEFAULT_MAX_SIZE];
+	}
+	
+	public ArrayBoundedListImpl(int size) {
+		elements = new Object[size];
+	}
 
 	@Override
 	public boolean add(T element) throws OverflowException {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.isFull()) {
+			//The list is full
+			throw new OverflowException("The list is full!");
+		}
+		//The element data into elementsNum of elements
+		this.elements[elementsNum] = element;
+		//Additional element number
+		this.elementsNum++;
+		//return true
+		return true;
 	}
 
 	@Override
 	public T search(T target) {
-		// TODO Auto-generated method stub
-		return null;
+		//Get get method in this class
+		return this.find(target);
 	}
 
 	@Override
 	public boolean contoins(T target) {
-		// TODO Auto-generated method stub
-		return false;
+		//Get get method in this class
+		T element = this.find(target);
+		//return true or false
+		return !(element == null);
+	}
+	
+	/**
+	 * Get a element data by target element.(The method's complexity is O(N))
+	 * @param target
+	 * @return target data
+	 */
+	public T find(T target) {
+		if(!this.isEmpty()) {
+			//The list is empty
+			int index = 0;
+			int arrNum = this.elementsNum;
+			//Loop and find target element
+			while(index < arrNum) {
+				//Get element of array by index
+				T element = ((T)elements[index]);
+				if(element.equals(target)) {
+					//Find is success and return the element object.
+					return element;
+				}
+				index++;
+			}
+		}
+		//When find is fail return null.
+		return null;
 	}
 
 	@Override
 	public boolean remove(T target) throws UnderflowException {
-		// TODO Auto-generated method stub
+		if(this.isEmpty()) {
+			//The list is empty
+			throw new UnderflowException("The list is empty!");
+		}
+		//The list is empty
+		int index = 0;
+		int arrNum = this.elementsNum;
+		//Loop and find target element
+		while(index < arrNum) {
+			//Get element of array by index
+			T element = ((T)elements[index]);
+			if(element.equals(target)) {
+				elements[index] = elements[arrNum - 1];
+				this.elementsNum--;
+				return true;
+			}
+			index++;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		return (this.elementsNum == elements.length);
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return (this.elementsNum == 0);
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.elementsNum;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer("");
+		if(!this.isEmpty()) {
+			//The list is empty
+			int index = 0;
+			int arrNum = this.elementsNum;
+			//Loop and find target element
+			while(index < arrNum) {
+				//Get element and append into the sb variable
+				sb.append(elements[index]);
+				index++;
+			}
+		}
+		//Return the string of sb
+		return sb.toString();
 	}
 
 }
---
+++ src/com/pro/test/DoTest.java
@@ -0,0 +1,24 @@
+package com.pro.test;
+
+import com.pro.imple.ArrayBoundedListImpl;
+
+public class DoTest {
+
+	public static void main(String[] args) {
+		ArrayBoundedListImpl<String> list = new ArrayBoundedListImpl<String>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		list.add("e");
+		list.add("f");
+		list.remove("c");
+		System.out.println(list.toString());
+		System.out.println(list.search("b"));
+		System.out.println(list.search("q"));
+		System.out.println(list.contoins("b"));
+		System.out.println(list.contoins("q"));
+		System.out.println(list.size());
+	}
+
+}
