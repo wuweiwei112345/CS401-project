9adf6f2c01b9f2fd9077c31b397c8448172c0fed
wuwei <wudawei112345>
2020-11-15 14:39:19 +0800

    ArrayUnBoundedListImpl 实现 接口 提交

--- src/com/pro/imple/ArrayUnBoundedListImpl.java
+++ src/com/pro/imple/ArrayUnBoundedListImpl.java
@@ -1,11 +1,55 @@
 package com.pro.imple;
 
+import com.pro.exception.OverflowException;
+import com.pro.exception.UnderflowException;
 import com.pro.inter.ListInterface;
 
 /**
- * @describe 数组无界列表实现
+ * @describe The implements is array-based's unbounded list
  * @author wuwei
  */
-public class ArrayUnBoundedListImpl implements ListInterface {
+public class ArrayUnBoundedListImpl<T> implements ListInterface<T> {
+
+	@Override
+	public boolean add(Object element) throws OverflowException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object search(Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contoins(Object target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Object target) throws UnderflowException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
