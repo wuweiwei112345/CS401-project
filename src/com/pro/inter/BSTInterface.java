bf32e5a2067d301d25bda57995d30f6864ddcd3b
wuwei <wudawei112345>
2020-11-17 09:58:35 +0800

    Binary search tree interface

---
+++ src/com/pro/inter/BSTInterface.java
@@ -0,0 +1,37 @@
+package com.pro.inter;
+
+import java.util.Iterator;
+
+/**
+ * @describe Binary search tree interface
+ * @author Wei Wu
+ * @param <T>
+ */
+public interface BSTInterface<T> extends ListInterface<T>, Iterable<T> {
+
+	/**
+	 * The Order of iterator 
+	 * @author wuwei
+	 */
+	public enum Traversal {Inorder,Preorder,Postorder};
+	
+	/**
+	 * Get least node info in the tree
+	 * @author wuwei
+	 */
+	public T min();
+	
+	/**
+	 * Get maximal node info in the tree
+	 * @author wuwei
+	 */
+	public T max();
+	
+	/**
+	 * Get iterator with this tree
+	 * @param orderType
+	 * @return Iterator
+	 */
+	public Iterator<T> getIterator(Traversal orderType);
+	
+}
