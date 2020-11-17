a5af404965342330ad75621c601a3ad32ffb4c70
wuwei <wudawei112345>
2020-11-17 18:21:49 +0800

    SimpleFactory 简单 工厂 类 及 测试

---
+++ src/com/pro/imple/SimpleFactory.java
@@ -0,0 +1,77 @@
+package com.pro.imple;
+
+import com.pro.inter.ListInterface;
+
+/**
+ * @describe For create instantiation of object
+ * @author Wei Wu
+ */
+public class SimpleFactory {
+
+	public static ListInterface<String> createListImplByString(String code,int maxSize) {
+		ListInterface<String> list = null;
+		switch (code) {
+			case "000000":
+				list = new ArrayBoundedListImpl<String>(maxSize);
+				break;
+			case "000001":
+				list = new ArrayBoundedListSortedImpl<String>(maxSize);
+				break;
+			case "000002":
+				list = new ArrayUnBoundedListImpl<String>();
+				break;
+			case "000003":
+				list = new ArrayUnBoundedListSortedImpl<String>();
+				break;
+			case "000005":
+				list = null;
+				break;
+			case "000006":
+				list = null;
+				break;
+			case "000007":
+				list = null;
+				break;
+			case "000008":
+				list = null;
+				break;
+			default:
+				break;
+			}
+		return list;
+	}
+	
+	public static ListInterface<Integer> createListImplByInteger(String code,int maxSize) {
+		ListInterface<Integer> list = null;
+		switch (code) {
+			case "000000":
+				list = new ArrayBoundedListImpl<Integer>(maxSize);
+				break;
+			case "000001":
+				list = new ArrayBoundedListSortedImpl<Integer>(maxSize);
+				break;
+			case "000002":
+				list = new ArrayUnBoundedListImpl<Integer>();
+				break;
+			case "000003":
+				list = new ArrayUnBoundedListSortedImpl<Integer>();
+				break;
+			case "000005":
+				list = null;
+				break;
+			case "000006":
+				list = null;
+				break;
+			case "000007":
+				list = null;
+				break;
+			case "000008":
+				list = null;
+				break;
+			default:
+				break;
+			}
+		return list;
+	}
+	
+}
--- src/com/pro/test/DoTest.java
+++ src/com/pro/test/DoTest.java
@@ -1,39 +1,35 @@
 package com.pro.test;
 
-import java.util.Iterator;
-
-import com.pro.imple.BinarySearchTree;
+import com.pro.imple.SimpleFactory;
+import com.pro.inter.ListInterface;
 
 public class DoTest {
 
 	public static void main(String[] args) {
-		BinarySearchTree<String> tree = new BinarySearchTree<String>();
-		tree.add("a");
-		tree.add("b");
-		tree.add("c");
-		tree.add("f");
-		tree.add("d");
-		tree.add("1");
-		tree.add("2");
-		tree.add("3");
-		tree.add("4");
-		tree.add("5");
-		tree.add("6");
-		tree.add("7");
-		tree.add("8");
-		tree.add("9");
-		tree.add("10");
-		tree.add("11");
-		tree.remove("2");
-		Iterator<String> iterator = tree.iterator();
-		while(iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
-		System.out.println(tree.search("b"));
-		System.out.println(tree.search("q"));
-		System.out.println(tree.contoins("b"));
-		System.out.println(tree.contoins("q"));
-		System.out.println(tree.size());
+		ListInterface<Integer> list = SimpleFactory.createListImplByInteger("000003",20);
+		list.add(1);
+		list.add(8);
+		list.add(6);
+		list.add(100);
+		list.add(124);
+		list.add(5);
+		list.add(7);
+		list.add(15);
+		list.add(67);
+		list.add(19);
+		list.add(6);
+		list.add(18);
+		list.add(11);
+		list.add(27);
+		list.add(40);
+		list.add(16);
+		list.remove(6);
+		System.out.println(list.toString());
+		System.out.println(list.search(8));
+		System.out.println(list.search(10));
+		System.out.println(list.contoins(8));
+		System.out.println(list.contoins(10));
+		System.out.println(list.size());
 	}
 
 }
