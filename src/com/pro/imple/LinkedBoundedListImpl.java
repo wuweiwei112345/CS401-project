1e8a33953725855e0a9d5b8e275eb0ce3de4b51f
wuwei <wudawei112345>
2020-11-25 10:20:57 +0800

    提交 ，防止 出错

--- src/com/pro/imple/LinkedBoundedListImpl.java
+++ src/com/pro/imple/LinkedBoundedListImpl.java
@@ -1,11 +1,55 @@
 package com.pro.imple;
 
+import com.pro.exception.OverflowException;
+import com.pro.exception.UnderflowException;
 import com.pro.inter.ListInterface;
 
 /**
  * @describe 链表有界列表实现
  * @author wuwei
  */
-public class LinkedBoundedListImpl implements ListInterface {
+public class LinkedBoundedListImpl<T> implements ListInterface<T> {
+
+	@Override
+	public boolean add(T element) throws OverflowException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T search(T target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contoins(T target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(T target) throws UnderflowException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
