d213040cee2a57151b76baadc008525383a34d2a
allyy <allyy@192.168.198.1>
2020-11-29 15:50:13 +0800

    无界链表的数据结构

--- src/com/pro/imple/LinkedUnBoundedListImpl.java
+++ src/com/pro/imple/LinkedUnBoundedListImpl.java
@@ -4,34 +4,99 @@ import com.pro.exception.OverflowException;
 import com.pro.exception.UnderflowException;
 import com.pro.inter.ListInterface;
 
+import com.pro.entity.LLNode;
+
 /**
  * @describe 链表无界列表实现
  * @author wuwei
  */
-public class LinkedUnBoundedListImpl<T> implements ListInterface<T> {
-
+public class LinkedUnBoundedListImpl<T extends Comparable<T>> implements ListInterface<T> {
+//	private int maxSize;
+//	private final int defaultMaxSize = 10;
+	private int numElements;
+	private LLNode<T> head;
+	private boolean found;
+	private LLNode<T> location;
+	private LLNode<T> previous;
+	private int index;
+//	private LLNode<T> currentNode;
+//	private int listNum;
+//	private LLNode<T> elements;
+	
 	@Override
 	public boolean add(T element) throws OverflowException {
 		// TODO Auto-generated method stub
-		return false;
+		LLNode<T> newNode = new LLNode<T>(element);
+		if(this.isEmpty())
+		{
+			this.head = newNode;
+			numElements++;
+			return true;
+		}
+
+		LLNode<T> currentNode = this.head;
+		while(currentNode.getNext() != null)
+		{
+			currentNode = currentNode.getNext();
+		}
+		currentNode.setNext(newNode);
+		numElements++;
+		return true;
 	}
 
+	public int find(T element) {
+		this.found = false;
+		this.location = null;
+		this.previous = null;
+		if(!(isEmpty()))
+		{
+			LLNode<T> currentNode = this.head;
+			while(currentNode != null)
+			{
+				if(element.compareTo(currentNode.getInfo()) == 0)
+				{
+					this.found = true;
+					this.location = currentNode;
+					break;
+				}else{
+					this.previous = currentNode;
+					currentNode = currentNode.getNext();
+				}
+			}
+		}
+		return index;
+    }
+	
 	@Override
 	public T search(T target) {
 		// TODO Auto-generated method stub
-		return null;
+		this.find(target);
+		if(this.location != null)
+		{
+			return this.location.getInfo();
+		}else{
+			return null;
+		}
 	}
-
+	
 	@Override
 	public boolean contain(T target) {
 		// TODO Auto-generated method stub
-		return false;
+		this.search(target);
+		return this.found;
 	}
 
 	@Override
 	public boolean remove(T target) throws UnderflowException {
 		// TODO Auto-generated method stub
-		return false;
+		this.find(target);
+        if(this.found == true)
+        {
+        	previous.setNext(location.getNext());
+        	return true;
+        }else {
+        	return false;
+        }
 	}
 
 	@Override
@@ -43,13 +108,27 @@ public class LinkedUnBoundedListImpl<T> implements ListInterface<T> {
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return this.numElements == 0;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.numElements;
 	}
+	
+    public String toString() {
+    	StringBuffer sb = new StringBuffer("[");
+    	
+        LLNode<T> currentNode = this.head;
+        while(currentNode != null)
+   		{
+   			sb.append(",").append(currentNode.getInfo());
+   			currentNode = currentNode.getNext();
+   		}
+        
+        sb.append("]");
+        return sb.toString().replaceFirst(",", "");
+    	}
+  	}
 
-}
