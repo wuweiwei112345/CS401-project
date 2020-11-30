ddfcf795a1f31d7179c9dcb11a7d0d989fe5d9b1
allyy <allyy@192.168.198.1>
2020-11-30 10:34:28 +0800

    节点文件

--- src/com/pro/entity/LLNode.java
+++ src/com/pro/entity/LLNode.java
@@ -1,5 +1,27 @@
 package com.pro.entity;
 
 public class LLNode<T> {
-	
+	protected LLNode<T> next;
+    protected T info;
+
+    public LLNode(T info) {
+        this.info = info;
+        next = null;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setNext(LLNode<T> link) {
+        this.next = link;
+    }
+
+    public LLNode<T> getNext() {
+        return next;
+    }
 }
